4e64f77844c6bb3fe544b6609af94f3d326c7a0a
Andy Osei <anyosei36@gmail.com>
2022-12-12 00:12:53 +0000

    cprod-1359 update mainnet token list

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,22 +3,24 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true,
+    node: true
   },
-  plugins: ["@typescript-eslint/eslint-plugin", "prettier"],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:jest/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   reportUnusedDisableDirectives: true,
   parserOptions: {
     /* enabling "project" field is a performance hit
         https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/TYPED_LINTING.md#performance
       */
-    sourceType: "module",
+    sourceType: 'module'
   },
-  rules: {},
+  rules: {
+    'typescript-eslint/no-non-null-assertion': 'off'
+  }
 };
