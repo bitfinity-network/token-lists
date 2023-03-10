39e0623ea0621993c9e642395a4a8e5cf0db3024
Shivam Kaushik <shivam@infinityswap.one>
2023-03-10 20:21:19 +0530

    EPROD-171 Add faucet token sync

---
+++ src/fetchFaucetTokens.js
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const tokenFilePath =
+  process.env.IC_ENVIRON === 'local' || process.env.IC_ENVIRON === 'testnet'
+    ? './evm.tokenlist.json'
+    : './evm.tokenlist.testnet.json';
+
+const FAUCET_TOKEN_URL =
+  'https://storage.googleapis.com/evmc/Addresses/logs/tokenAddresses.json';
+
+const updateTokenListJson = async (data, path) => {
+  fs.writeFile(
+    new URL(path, import.meta.url),
+    JSON.stringify(data),
+    function (err) {
+      if (err) throw err;
+      console.log('complete');
+    }
+  );
+};
+
+const fetchTokensFromGc = async () => {
+  try {
+    const response = await axios.get(FAUCET_TOKEN_URL, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
+    const tokens = [];
+    Object.keys(response.data.tokens).forEach((i) => {
+      tokens.push({
+        ...response.data.tokens[i],
+        logo: `https://raw.githubusercontent.com/infinity-swap/token-lists/main/logos/faucet.png`
+      });
+    });
+    return tokens;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+(async () => {
+  const faucetTokens = await fetchTokensFromGc();
+  updateTokenListJson(
+    {
+      name: 'InfinitySwap Faucet Tokens',
+      tokens: faucetTokens
+    },
+    tokenFilePath
+  );
+})();
